4f6c3d79867eb8604d5072006611156ba6f7aef0
JoaoMazzoni <joao.mazzoni@5by5.com>
2024-04-18 15:58:33 -0300

    Add project files.

---
+++ CalcularIdade/Program.cs
@@ -0,0 +1,24 @@
+﻿int idade;
+
+Console.WriteLine("Digite a idade: ");
+idade = int.Parse(Console.ReadLine());
+
+if (idade < 0) 
+{
+    Console.WriteLine("Idade inválida.");
+}
+
+else
+{
+    if (idade < 18) 
+    {
+        Console.WriteLine("{0} anos, menor de idade.", idade);
+    } 
+
+    else
+    {
+        Console.WriteLine("{0} anos, maior de idade.", idade);
+    }
+
+
+}   
